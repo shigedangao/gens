f93ead64fbd416f87eec4a585840d8e021f05ba8
shigedangao <marc.inthaamnouay@gmail.com>
2025-11-29 21:41:14 +0100

    add small readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gens
+
+A set of small python script to test the https://huggingface.co/Tongyi-MAI/Z-Image-Turbo on runpod
--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import runpod
 import os
 import time
 
-endpoint_id = "xxxx"
+endpoint_id = os.getenv("RUNPOD_ENDPOINT_ID")
 payload = {
     "input": {
         "prompt": "Young tunisian women from la marsa, light brown long hair, with small lip and a small forehead looking at the camera, high quality, 8k, detailed, intricate, elegant with a smiling face and a flower on the left side of her head. Being in the center of tunis, sidi bou said in a sunny day eating a traditional tunisian dish called kefteji, wearing a modern casual outfit with a light blue dress with flower pattern, surrounded by beautiful white and blue buildings with bougainvillea flowers, vibrant colors, cinematic lighting, shallow depth of field, photo taken with an iPhone."
